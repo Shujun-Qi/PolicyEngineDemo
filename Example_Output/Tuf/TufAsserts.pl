14c8440db0400e172412b61bc806e4cb30588799
Chainguard-Shujun-Workstation <shujun@Chainguard-Shujun-Workstations-MacBook-Pro.local>
2023-06-08 00:13:35 -0400

    Tuf implementation

--- Example_Output/Tuf/TufAsserts.pl
+++
@@ -1,4 +0,0 @@
-:- consult('Policy/tuf.pl').
-tuf:root('8a1c4a3ac2d515dec982ba9910c5fd79b91ae57f625b9cff25d06bf0a61c1758').
-tuf:sign('8a1c4a3ac2d515dec982ba9910c5fd79b91ae57f625b9cff25d06bf0a61c1758', '8f88e2ba48b412c3843e9bb26e1b6f8fc9e98aceb0fbaa97ba37b4c98717d7ab', 'null', 'snapshot.json').
-tuf:metadata('59a4df8af818e9ed7abe0764c0b47b4240952aa0d179b5b78346c470ac30278d', '8f88e2ba48b412c3843e9bb26e1b6f8fc9e98aceb0fbaa97ba37b4c98717d7ab', 'null', 'target.json').
